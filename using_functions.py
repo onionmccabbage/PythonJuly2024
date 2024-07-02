51b69671bdf9dd9d736578f5e89cfd4472acd997
onionmccabbage <onionmccabbage@live.com>
2024-07-02 10:44:13 +0100

    using input

--- using_functions.py
+++ using_functions.py
@@ -8,7 +8,16 @@ def checkEven(n):
     else:
         return False
 
+def checkNum():
+    '''We ask the user for a value and return the numeric part or else a string'''
+    r = input('Please enter a value: ')
+    # here we check if they entered a numeric value (careful every input is ALWAYS a string)
+    if r.isnumeric: # we check if the string loks like a numeric value
+        return int(r) # convert it to an integer
+    else:
+        return 'that is not an integer value'
 
 # we may call our functions whenever we like
 result = checkEven(42) # at this point our funtion will be executed
 print(result)
+checkNum()
