7c4983d384748e21ec4de853b3d0ae66148a0e3e
onionmccabbage <onionmccabbage@live.com>
2024-07-02 12:13:06 +0100

    usling run loop

--- run_loop.py
+++ run_loop.py
@@ -4,9 +4,15 @@ from random import randint
 
 # here is a run loop
 while True:
-    r = randint(0,10)
-    print(r)
-    if r==5:
-        break # this will stop the while loop - our code will end
+    '''ask the user for a value. If they enter q we quit. Otherwise we capitalize their input'''
+    r = input('Please enter a value')
+    q = r.upper() # force the string to uppercase (also q.lower())
+    if q == 'Q':
+        break
+    # r = randint(0,10)
+    else:
+        print(f'{r} in upperxcase is {q})
+    # if r==5:
+    #     break # this will stop the while loop - our code will end
 
 
