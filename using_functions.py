d4f3bc7ddb7bb9d12f12e40eb08365abda1e25f4
onionmccabbage <onionmccabbage@live.com>
2024-07-02 10:45:56 +0100

    using input and isnumeric()

--- using_functions.py
+++ using_functions.py
@@ -12,7 +12,7 @@ def checkNum():
     '''We ask the user for a value and return the numeric part or else a string'''
     r = input('Please enter a value: ')
     # here we check if they entered a numeric value (careful every input is ALWAYS a string)
-    if r.isnumeric: # we check if the string loks like a numeric value
+    if r.isnumeric(): # we check if the string looks like a numeric value
         return int(r) # convert it to an integer
     else:
         return 'that is not an integer value'
@@ -20,4 +20,4 @@ def checkNum():
 # we may call our functions whenever we like
 result = checkEven(42) # at this point our funtion will be executed
 print(result)
-checkNum()
+print(  checkNum()  )
