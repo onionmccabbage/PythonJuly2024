16ebaca03b12a2052ba6d84c36013bc2477cc819
onionmccabbage <onionmccabbage@live.com>
2024-07-02 11:47:12 +0100

    preventing imported code from running

--- my_structure/proj.py
+++ my_structure/proj.py
@@ -1,5 +1,6 @@
 # we may need to import other features from python
 import random # this is a library in python
+# CAREFUL when you import another module it runs everything in that module
 import util # this imports our own module
 
 def makeRandomInt():
@@ -7,11 +8,12 @@ def makeRandomInt():
     r = random.randint(0, 100)
     return r
 
-# we may call our function whenever we need it
-n = makeRandomInt() # this will grab a random integer
-print(n)
-# we can use our utility fnction to check if the random number is alaos a prime number
-isPrime = util.checkIfPrime(n)
-# we may choose to format the printed output
-# f'' creates a formatted string. We may inject any value using {}
-print(f'The random integer {n} is prime:{isPrime}')
+if __name__ == '__main__':
+    # we may call our function whenever we need it
+    n = makeRandomInt() # this will grab a random integer
+    print(n)
+    # we can use our utility fnction to check if the random number is alaos a prime number
+    isPrime = util.checkIfPrime(n)
+    # we may choose to format the printed output
+    # f'' creates a formatted string. We may inject any value using {}
+    print(f'The random integer {n} is prime:{isPrime}')
--- my_structure/util.py
+++ my_structure/util.py
@@ -6,8 +6,11 @@ def checkIfPrime(n):
         return True
     else:
         return False
-    
-# we may exercise the code to see if it works as expected
-c = 43
-answer = checkIfPrime(c) # True
-print(f'The value {c} is prime:{answer} (inside util.py)')
+
+# we often use this line to prevent code running if this module gets imported elsewhere
+if __name__ == '__main__':
+    # we may exercise the code to see if it works as expected
+    # this code only runs when we execute thsi module directly (not if imported)
+    c = 43
+    answer = checkIfPrime(c) # True
+    print(f'The value {c} is prime:{answer} (inside util.py)')
