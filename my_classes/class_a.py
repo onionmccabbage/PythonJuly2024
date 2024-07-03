19ada4445014aeb12d6de1403a4735662cce0347
onionmccabbage <onionmccabbage@live.com>
2024-07-03 10:08:55 +0100

    class with properties and methods

--- my_classes/class_a.py
+++ my_classes/class_a.py
@@ -13,13 +13,20 @@ class Volunteer:
     hours (a positive float)
     rate(a positive float)'''
     # NB every function inside a class MUST take 'self' as an argument
-    def __init__(self, n): # a bit like a constructor in Java
+    def __init__(self, n, h, r): # a bit like a constructor in Java
         self.name = n # self refers to whatever instance we are building
+        self.hours = h # NB each property may be of ANY data type (number, str, list, tuple, dict...)
+        self.rate = r
+    # we may also give our class methods (stuff it can do)
+    def totalGain(self):
+        '''calculate the total of hours*rate'''
+        gain = self.hours*self.rate
+        return gain
 
 if __name__ == '__main__':
     # we may have as many instances of a class as we need
-    e = Volunteer('Edith') # we now have an instance of our class
-    f = Volunteer('Floella') # ...another instance
-    print(e.name, f.name)
+    e = Volunteer('Edith', 36.2, 6.0) # we now have an instance of our class
+    f = Volunteer('Floella', 12.7, 3.6) # ...another instance
+    print(f'{e.name} did {e.hours} voluntary work at {e.rate} earning {e.totalGain()}')
 
 
