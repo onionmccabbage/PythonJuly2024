7318da6b4f9666a661f112405564f5fd6fffb869
onionmccabbage <onionmccabbage@live.com>
2024-07-03 11:54:35 +0100

    some simple utility functions

---
+++ functional_programming/fn.py
@@ -0,0 +1,25 @@
+# any imports tend to go at the top of the module
+import datetime
+
+# define any function next
+def addThem(a, b):
+    '''return the sum of a+b'''
+    return a+b
+
+def isOdd(n):
+    '''return True if n is odd (False otherwise)'''
+    if n%2 !=0: # % is integer division
+        return True # we have an odd number
+    else:
+        return False
+
+
+
+
+
+# we may also choose to declare classes...
+# (none needed here)
+
+
+if __name__ == '__main__':
+    pass
