e83ee45e2c2f1fdba5a68be7904c26276d5e27a2
onionmccabbage <onionmccabbage@live.com>
2024-07-03 10:18:27 +0100

    using get/set decorators

--- my_classes/class_a.py
+++ my_classes/class_a.py
@@ -17,6 +17,17 @@ class Volunteer:
         self.name = n # self refers to whatever instance we are building
         self.hours = h # NB each property may be of ANY data type (number, str, list, tuple, dict...)
         self.rate = r
+    # we declare properties in order to validate our data
+    @property # this is called a decorator
+    def name(self): # this is the property getter
+        return self.__name # the double underscore will mangle this property
+    @name.setter
+    def name(self, new_name): # this is the property setter
+        '''only allow non-empty string for name'''
+        if type(new_name) == str and new_name !='':
+            self.__name = new_name # all good, we can store the name
+        else:
+            raise TypeError('Name must be a non-empty string')
     # we may also give our class methods (stuff it can do)
     def totalGain(self):
         '''calculate the total of hours*rate'''
@@ -27,6 +38,6 @@ if __name__ == '__main__':
     # we may have as many instances of a class as we need
     e = Volunteer('Edith', 36.2, 6.0) # we now have an instance of our class
     f = Volunteer('Floella', 12.7, 3.6) # ...another instance
-    print(f'{e.name} did {e.hours} voluntary work at {e.rate} earning {e.totalGain()}')
+    print(f'{e.name} did {e.hours} voluntary work at {e.rate} earning €{e.totalGain():0.2f}')
 
 
