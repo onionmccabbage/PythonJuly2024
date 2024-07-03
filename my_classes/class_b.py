0191f9f32c13799e33e3cbb87fe2b3543ebc7957
onionmccabbage <onionmccabbage@live.com>
2024-07-03 11:37:18 +0100

    overriding the built in __str__ for nicely printed output

--- my_classes/class_b.py
+++ my_classes/class_b.py
@@ -6,8 +6,8 @@ class VolunteerGroup(Volunteer):
     '''we now have all the properties and methods inherited from 'Volunteer' '''
     def __init__(self, n, h, r, num_people): # a group is several people
         # we call the __init__ of the parent class
-        # super().__init__(n ,h, r)
-        # self.num_people = num_people # remember - this calls our setter function
+        super().__init__(n ,h, r)
+        self.num_people = num_people # remember - this calls our setter function
     @property
     def num_people(self):
         return self.__num_people # return the mangled value
@@ -18,6 +18,9 @@ class VolunteerGroup(Volunteer):
             self.__num_people = new_num_people
         else:
             self.__num_people = 2 # sensible default
+    # whenever we write a class we may choose to override the build in __str__ method for printing
+    def __str__(self):
+        return f'{self.name} has {self.num_people} volunteers who covered {self.hours} at {self.rate} gaining €{self.totalGain():0.2f}'
 
 # what does Python think this module is called.....?
 print(f'Python has assigned the following name to this module: {__name__}') # Python will ALWAYS assign a name to the running module
@@ -26,3 +29,6 @@ if __name__ == '__main__':
     # we may create instances
     grA = VolunteerGroup('Group A', 456.55, 0.44, 6)
     print(f'{grA.name} carried out {grA.hours} voluntary work among {grA.num_people} people')
+
+    # whenever we call 'print' it always uses the __str__ method
+    print(grA)
